7ea4e3d8c209cac930e8cb92f791c6e0d619727a
akimjlord-p <81712770+akimjlord-p@users.noreply.github.com>
2026-02-25 21:09:35 +0300

    feat & refac normal report sending

--- bot.py
+++ bot.py
@@ -22,13 +22,15 @@ dp.include_router(admin.router)
 async def send_weekly_report(bot: Bot) -> None:
 
     report_text = """Отчеты за последнюю неделю:\n"""
-    res_reports = []
+    all_users = []
     usernames = get_all_usernames()
     for username in usernames:
         reports = get_reports_by_username(username)
+        res_reports = []
         for report in reports:
             res_reports.append(f"Дата: {report.date} \n" + str(report.message))
-    report_text += "\n".join(res_reports)
+        all_users.append(f"Работник {username}" + '\n'.join(res_reports))
+    report_text += "\n".join(all_users)
     await bot.send_message(chat_id=MAIN_ID, text=report_text, reply_markup=get_kb(is_admin=True))
     logging.info("Report sent")
 
