387b9e8245b4281f58207bb9c9aaf57b52464a3e
akimjlord-p <81712770+akimjlord-p@users.noreply.github.com>
2026-02-25 19:09:25 +0300

    feat: report command

---
+++ handlers/report.py
@@ -0,0 +1,37 @@
+import logging
+from aiogram import Router, types, F
+from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from db import get_user_by_username, add_report_to_db
+from models import ReportBase
+
+
+router = Router()
+
+
+# ==============================
+#           FSM
+# ==============================
+
+class ReportFSM(StatesGroup):
+    report = State()
+
+
+@router.message(F.text.lower() == "/report")
+async def start_report(message: types.Message, state: FSMContext):
+    logging.info(f"User {message.from_user.username} start report successfully")
+    await state.set_state(ReportFSM.report)
+    await message.answer(text="Привет, введи текст отчета")
+
+
+@router.message(ReportFSM.report)
+async def get_report(message: types.Message, state: FSMContext):
+    logging.info(f"User {message.from_user.username} get report successfully")
+    data = await state.get_data()
+    report_text = data.get("report")
+    report = ReportBase(message=report_text, user_id=message.from_user.id)
+    add_report_to_db(report)
+    await message.answer(text="Ваш отчет успешно сохранён")
+
+
