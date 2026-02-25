272c247a33ce2d70b8fecbd46c2faf01be9dc32c
akimjlord-p <81712770+akimjlord-p@users.noreply.github.com>
2026-02-25 15:49:56 +0300

    refactor logging

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import logging
 from bot import main
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logging.basicConfig(filename='app.log', level=logging.DEBUG)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("app.log", encoding='utf-8'),
+        logging.StreamHandler()
+    ]
+)
 
 
 main()
