4ffeebc29cfc96d1748fb8d0d34b81fd77ce8d2b
akimjlord-p <81712770+akimjlord-p@users.noreply.github.com>
2026-03-02 13:06:00 +0300

    feat(bot) add scheduler daily and weeks reports

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from middlewares import AccessMiddleware, AdminMiddleware
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from keyboards import get_kb
 from handlers import admin, start, report
-from auto_mailings import send_weekly_report
+from auto_mailings import send_weekly_reports, send_daily_reports
 
 
 bot = Bot(token=BOT_TOKEN)
@@ -26,7 +26,7 @@ dp.include_router(admin.router)
 async def main() -> None:
     scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
 
-    scheduler.add_job(send_weekly_report, trigger="cron", day_of_week="sun", hour=9, minute=20, kwargs={"bot": bot})
-
+    scheduler.add_job(send_weekly_reports(bot), trigger="cron", day_of_week="sun", hour=9, minute=20, kwargs={"bot": bot})
+    scheduler.add_job(send_daily_reports, trigger="cron", hour=21, minute=0, kwargs={"bot": bot})
     scheduler.start()
     await dp.start_polling(bot)
