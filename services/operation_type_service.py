6ad90f90a52d60d2266c3796c610457ec2816c8a
akimjlord-p <81712770+akimjlord-p@users.noreply.github.com>
2026-03-22 01:14:22 +0300

    refac add synonyms func

--- services/operation_type_service.py
+++ services/operation_type_service.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from repositories.operation_repository import OperationRepository
-from db.models import OperationType
+from db.models import OperationType, OperationSynonym
 
 class OperationTypeService:
     def __init__(self, session: AsyncSession):
@@ -17,8 +17,18 @@ class OperationTypeService:
         operation_type = self.operation_repository.get_operation_type_by_name(operation_name)
         if operation_type:
             return None
-
         new_operation_type = await self.operation_repository.create_operation_type(operation_name)
         await self.session.commit()
         return new_operation_type
+
+
+    async def add_operation_type_synonyms(self, operation_type: OperationType, raw_synonyms: list[str]) -> list[OperationSynonym]:
+        synonyms: list[OperationSynonym] = []
+        for raw_synonym in raw_synonyms:
+            synonym = await self.operation_repository.add_operation_synonym(
+                operation_type.id,
+                raw_synonym
+            )
+            synonyms.append(synonym)
+        return synonyms
 
--- services/product_service.py
+++ services/product_service.py
@@ -17,13 +17,13 @@ class ProductService:
         await self.session.commit()
         return product
 
-    async def add_synonyms(self, product: Product, raw_product_synonyms: list[str]) -> list[ProductSynonym]:
-        product_synonyms: list[ProductSynonym] = []
-        for raw_product_synonym in raw_product_synonyms:
-            product_synonym = await self.operation_repository.add_product_synonym(
+    async def add_synonyms(self, product: Product, raw__synonyms: list[str]) -> list[ProductSynonym]:
+        synonyms: list[ProductSynonym] = []
+        for raw_synonym in raw__synonyms:
+            synonym = await self.operation_repository.add_product_synonym(
                 product.id,
-                raw_product_synonym)
+                raw_synonym)
 
-            product_synonyms.append(product_synonym)
+            synonyms.append(synonym)
         await self.session.commit()
-        return product_synonyms
+        return synonyms
